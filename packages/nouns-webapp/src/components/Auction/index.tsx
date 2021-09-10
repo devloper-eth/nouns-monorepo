be047d86a7f48d671a1bcd6042dc21c310661e5c
Ryan Floyd <32312309+MrRyanFloyd@users.noreply.github.com>
2021-09-10 00:40:13 -0400

    big sigh
    big sigh
    
    TO DO:
    - claim tokens functionality
    - include check to prevent user from bidding more than current vault amount. this is an uncaught error on nouns.wtf that should be handled
    - disable hover style or remove "place bid" when auction is finished or when on past auction screens
    
    STYLING TO DO:
     - parent container height when switching between current and past auctions.
    - mobile container height stretching to bottom
    - parent container height min height to remove large white space on larger desktop screens
    
    and more

--- packages/nouns-webapp/src/components/Auction/index.tsx
+++ packages/nouns-webapp/src/components/Auction/index.tsx
@@ -6,47 +6,42 @@ import { Auction as IAuction } from '../../wrappers/nounsAuction';
 import classes from './Auction.module.css';
 import { useEffect, useState } from 'react';
 import { INounSeed } from '../../wrappers/nounToken';
-import PartyActivity from '../PartyActivity';
-
+import { isNounderNoun } from '../../utils/nounderNoun';
+import NounderNounContent from '../NounderNounContent';
+import { ApolloError } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { auctionQuery } from '../../wrappers/subgraph';
+import { BigNumber } from 'ethers';
+import AuctionActivity from '../AuctionActivity';
 /* OLD IMPORTS - FLAGGED FOR DELETION */
-// import { useHistory } from 'react-router-dom';
-// import { useAppDispatch, useAppSelector } from '../../hooks';
-// import { isNounderNoun } from '../../utils/nounderNoun';
 // import {
 //   setNextOnDisplayAuctionNounId,
 //   setPrevOnDisplayAuctionNounId,
 // } from '../../state/slices/onDisplayAuction';
-// import NounderNounContent from '../NounderNounContent';
-// import { ApolloError } from '@apollo/client';
-// import { useQuery } from '@apollo/client';
-// import { auctionQuery } from '../../wrappers/subgraph';
-// import { BigNumber } from 'ethers';
-// import AuctionActivity from '../AuctionActivity';
+// import { useHistory } from 'react-router-dom';
+// import { useAppDispatch, useAppSelector } from '../../hooks';
+// import PartyActivity from '../PartyActivity';
 
-// const isNounderNoun = (nounId: BigNumber) => {
-//   return nounId.mod(10).eq(0) || nounId.eq(0);
-// };
+const prevAuctionsAvailable = (
+  loadingPrev: boolean,
+  errorPrev: ApolloError | undefined,
+  prevAuction: IAuction,
+) => {
+  return !loadingPrev && prevAuction !== undefined && !errorPrev;
+};
 
-// const prevAuctionsAvailable = (
-//   loadingPrev: boolean,
-//   errorPrev: ApolloError | undefined,
-//   prevAuction: IAuction,
-// ) => {
-//   return !loadingPrev && prevAuction !== undefined && !errorPrev;
-// };
-
-// const createAuctionObj = (data: any): IAuction => {
-//   const auction: IAuction = {
-//     amount: BigNumber.from(data.auction.amount),
-//     bidder: data.auction?.bidder?.id,
-//     endTime: data.auction.endTime,
-//     startTime: data.auction.startTime,
-//     length: data.auction.endTime - data.auction.startTime,
-//     nounId: data.auction.id,
-//     settled: data.auction.settled,
-//   };
-//   return auction;
-// };
+const createAuctionObj = (data: any): IAuction => {
+  const auction: IAuction = {
+    amount: BigNumber.from(data.auction.amount),
+    bidder: data.auction?.bidder?.id,
+    endTime: data.auction.endTime,
+    startTime: data.auction.startTime,
+    // length: data.auction.endTime - data.auction.startTime,
+    nounId: data.auction.id,
+    settled: data.auction.settled,
+  };
+  return auction;
+};
 
 const Auction: React.FC<{ auction: IAuction; bgColorHandler: (useGrey: boolean) => void }> =
   props => {
@@ -54,39 +49,39 @@ const Auction: React.FC<{ auction: IAuction; bgColorHandler: (useGrey: boolean) 
 
     const [onDisplayNounId, setOnDisplayNounId] = useState(currentAuction && currentAuction.nounId);
     const [lastAuctionId, setLastAuctionId] = useState(currentAuction && currentAuction.nounId);
-    // const [isLastAuction, setIsLastAuction] = useState(true);
-    // const [isFirstAuction, setIsFirstAuction] = useState(false);
+    const [isLastAuction, setIsLastAuction] = useState(true);
+    const [isFirstAuction, setIsFirstAuction] = useState(false);
 
     // Query onDisplayNounId auction. Used to display past auctions' data.
-    // const { data: dataCurrent } = useQuery(
-    //   auctionQuery(onDisplayNounId && onDisplayNounId.toNumber()),
-    // );
+    const { data: dataCurrent } = useQuery(
+      auctionQuery(onDisplayNounId && onDisplayNounId.toNumber()),
+    );
     // Query onDisplayNounId auction plus one. Used to determine nounder noun timestamp.
-    // const { data: dataNext } = useQuery(
-    //   auctionQuery(onDisplayNounId && onDisplayNounId.add(1).toNumber()),
-    // );
+    const { data: dataNext } = useQuery(
+      auctionQuery(onDisplayNounId && onDisplayNounId.add(1).toNumber()),
+    );
 
     // Query onDisplayNounId auction minus one. Used to cache prev auction + check if The Graph queries are functional.
-    // const {
-    //   loading: loadingPrev,
-    //   data: dataPrev,
-    //   error: errorPrev,
-    // } = useQuery(auctionQuery(onDisplayNounId && onDisplayNounId.sub(1).toNumber()), {
-    //   pollInterval: 10000,
-    // });
+    const {
+      loading: loadingPrev,
+      data: dataPrev,
+      error: errorPrev,
+    } = useQuery(auctionQuery(onDisplayNounId && onDisplayNounId.sub(1).toNumber()), {
+      pollInterval: 10000,
+    });
 
     /**
      * Auction derived from `onDisplayNounId` query
      */
-    // const auction: IAuction = dataCurrent && dataCurrent.auction && createAuctionObj(dataCurrent);
+    const auction: IAuction = dataCurrent && dataCurrent.auction && createAuctionObj(dataCurrent);
     /**
      * Auction derived from `onDisplayNounId.add(1)` query
      */
-    // const nextAuction: IAuction = dataNext && dataNext.auction && createAuctionObj(dataNext);
+    const nextAuction: IAuction = dataNext && dataNext.auction && createAuctionObj(dataNext);
     /**
      * Auction derived from `onDisplayNounId.sub(1)` query
      */
-    // const prevAuction: IAuction = dataPrev && dataPrev.auction && createAuctionObj(dataPrev);
+    const prevAuction: IAuction = dataPrev && dataPrev.auction && createAuctionObj(dataPrev);
 
     const loadedNounHandler = (seed: INounSeed) => {
       bgColorHandler(seed.background === 0);
@@ -99,17 +94,17 @@ const Auction: React.FC<{ auction: IAuction; bgColorHandler: (useGrey: boolean) 
       }
     }, [onDisplayNounId, currentAuction, lastAuctionId]);
 
-    // const auctionHandlerFactory = (nounIdMutator: (prev: BigNumber) => BigNumber) => () => {
-    //   setOnDisplayNounId(prev => {
-    //     const updatedNounId = nounIdMutator(prev);
-    //     setIsFirstAuction(updatedNounId.eq(0) ? true : false);
-    //     setIsLastAuction(updatedNounId.eq(currentAuction && currentAuction.nounId) ? true : false);
-    //     return updatedNounId;
-    //   });
-    // };
+    const auctionHandlerFactory = (nounIdMutator: (prev: BigNumber) => BigNumber) => () => {
+      setOnDisplayNounId(prev => {
+        const updatedNounId = nounIdMutator(prev);
+        setIsFirstAuction(updatedNounId.eq(0) ? true : false);
+        setIsLastAuction(updatedNounId.eq(currentAuction && currentAuction.nounId) ? true : false);
+        return updatedNounId;
+      });
+    };
 
-    // const prevAuctionHandler = auctionHandlerFactory((prev: BigNumber) => prev.sub(1));
-    // const nextAuctionHandler = auctionHandlerFactory((prev: BigNumber) => prev.add(1));
+    const prevAuctionHandler = auctionHandlerFactory((prev: BigNumber) => prev.sub(1));
+    const nextAuctionHandler = auctionHandlerFactory((prev: BigNumber) => prev.add(1));
 
     const nounContent = (
       <div className={classes.nounWrapper}>
@@ -123,39 +118,40 @@ const Auction: React.FC<{ auction: IAuction; bgColorHandler: (useGrey: boolean) 
       </div>
     );
 
-    // const auctionActivityContent = (auction: IAuction, displayGraphDepComps: boolean) => (
-    //   <AuctionActivity
-    //     auction={auction}
-    //     isFirstAuction={isFirstAuction}
-    //     isLastAuction={isLastAuction}
-    //     onPrevAuctionClick={prevAuctionHandler}
-    //     onNextAuctionClick={nextAuctionHandler}
-    //     displayGraphDepComps={displayGraphDepComps}
-    //   />
-    // );
+    const auctionActivityContent = (auction: IAuction, displayGraphDepComps: boolean) => (
+      <AuctionActivity
+        auction={auction}
+        isFirstAuction={isFirstAuction}
+        isLastAuction={isLastAuction}
+        onPrevAuctionClick={prevAuctionHandler}
+        onNextAuctionClick={nextAuctionHandler}
+        displayGraphDepComps={displayGraphDepComps}
+      />
+    );
 
-    // const currentAuctionActivityContent =
-    //   currentAuction &&
-    //   auctionActivityContent(
-    //     currentAuction,
-    //     onDisplayNounId && prevAuctionsAvailable(loadingPrev, errorPrev, prevAuction), // else check if prev auct is avail
-    //   );
+    const currentAuctionActivityContent =
+      currentAuction &&
+      auctionActivityContent(
+        currentAuction,
+        onDisplayNounId && prevAuctionsAvailable(loadingPrev, errorPrev, prevAuction), // else check if prev auct is avail
+      );
 
-    // const pastAuctionActivityContent =
-    //   auction &&
-    //   auctionActivityContent(auction, prevAuctionsAvailable(loadingPrev, errorPrev, prevAuction));
+    const pastAuctionActivityContent =
+      auction &&
+      auctionActivityContent(auction, prevAuctionsAvailable(loadingPrev, errorPrev, prevAuction));
 
     // FLAGGED FOR DELETION - Will we ever need to show nounder's nouns?
-    // const nounderNounContent = nextAuction && (
-    //   <NounderNounContent
-    //     mintTimestamp={nextAuction.startTime}
-    //     nounId={onDisplayNounId}
-    //     isFirstAuction={isFirstAuction}
-    //     isLastAuction={isLastAuction}
-    //     onPrevAuctionClick={prevAuctionHandler}
-    //     onNextAuctionClick={nextAuctionHandler}
-    //   />
-    // );
+    const nounderNounContent = nextAuction && (
+      <NounderNounContent
+        // mintTimestamp={nextAuction.startTime}
+        nounId={onDisplayNounId}
+        isFirstAuction={isFirstAuction}
+        isLastAuction={isLastAuction}
+        onPrevAuctionClick={prevAuctionHandler}
+        onNextAuctionClick={nextAuctionHandler}
+        displayGraphDepComps={prevAuctionsAvailable(loadingPrev, errorPrev, prevAuction)}
+      />
+    );
 
     return (
       <Container fluid={true} className={classes.pageContentWrapper}>
@@ -166,9 +162,24 @@ const Auction: React.FC<{ auction: IAuction; bgColorHandler: (useGrey: boolean) 
           <Col lg={2} />
           <Col xs={12} lg={5} className={`align-self-end ${classes.noPaddingMargin}`}>
             {onDisplayNounId ? nounContent : loadingNoun}
+            {/* {onDisplayNounId ? currentAuctionActivityContent : pastAuctionActivityContent} */}
           </Col>
-          <Col xs={12} lg={4} className={`align-self-end  ${classes.partyActivityTopSpacer}`}>
-            <PartyActivity auction={currentAuction} />
+          {/* align-self-end   */}
+          <Col
+            xs={12}
+            lg={4}
+            className={
+              isLastAuction
+                ? classes.currentAuctionActivityContainer
+                : classes.pastAuctionActivityContainer
+            }
+          >
+            {onDisplayNounId && isNounderNoun(onDisplayNounId)
+              ? nounderNounContent
+              : isLastAuction
+              ? currentAuctionActivityContent
+              : pastAuctionActivityContent}
+            {/* {isLastAuction ? currentAuctionActivityContent : pastAuctionActivityContent} */}
           </Col>
           <Col lg={2} />
 
--- packages/nouns-webapp/src/components/NounderNounContent/index.tsx
+++ packages/nouns-webapp/src/components/NounderNounContent/index.tsx
@@ -1,87 +1,132 @@
 import { Col, Row } from 'react-bootstrap';
 import { BigNumber } from 'ethers';
-import AuctionActivityWrapper from '../AuctionActivityWrapper';
 import AuctionNavigation from '../AuctionNavigation';
-import AuctionActivityNounTitle from '../AuctionActivityNounTitle';
-import AuctionActivityDateHeadline from '../AuctionActivityDateHeadline';
-import { Link } from 'react-router-dom';
-import nounContentClasses from './NounderNounContent.module.css';
-import auctionBidClasses from '../AuctionActivity/BidHistory.module.css';
-import bidBtnClasses from '../BidHistoryBtn//BidHistoryBtn.module.css';
-import auctionActivityClasses from '../AuctionActivity/AuctionActivity.module.css';
-import CurrentBid, { BID_N_A } from '../CurrentBid';
+import classes from './NounderNounContent.module.css';
+// import AuctionActivityNounTitle from '../AuctionActivityNounTitle';
+// import AuctionActivityDateHeadline from '../AuctionActivityDateHeadline';
+// import { Link } from 'react-router-dom';
+// import nounContentClasses from './NounderNounContent.module.css';
+// import auctionBidClasses from '../AuctionActivity/BidHistory.module.css';
+// import bidBtnClasses from '../BidHistoryBtn//BidHistoryBtn.module.css';
+// import auctionActivityClasses from '../AuctionActivity/AuctionActivity.module.css';
+// import CurrentBid, { BID_N_A } from '../CurrentBid';
+// import AuctionActivityWrapper from '../AuctionActivityWrapper';
 
 const NounderNounContent: React.FC<{
-  mintTimestamp: BigNumber;
+  // mintTimestamp: BigNumber;
   nounId: BigNumber;
   isFirstAuction: boolean;
   isLastAuction: boolean;
   onPrevAuctionClick: () => void;
   onNextAuctionClick: () => void;
+  displayGraphDepComps: boolean;
 }> = props => {
   const {
-    mintTimestamp,
+    // mintTimestamp,
     nounId,
     isFirstAuction,
     isLastAuction,
     onPrevAuctionClick,
     onNextAuctionClick,
+    displayGraphDepComps,
   } = props;
 
   return (
-    <AuctionActivityWrapper>
-      <div className={auctionActivityClasses.informationRow}>
-        <Row className={auctionActivityClasses.activityRow}>
-          <Col lg={12}>
-            <AuctionActivityDateHeadline startTime={mintTimestamp} />
-          </Col>
-          <Col lg={12} className={auctionActivityClasses.colAlignCenter}>
-            <AuctionActivityNounTitle nounId={nounId} />
+    <>
+      <div className={classes.partyPaperContainer}>
+        <div className={classes.nounIdContainer}>
+          <h1 className={classes.nounIdText}>{`Noun ${nounId}`}</h1>
+          {displayGraphDepComps && (
             <AuctionNavigation
               isFirstAuction={isFirstAuction}
               isLastAuction={isLastAuction}
               onNextAuctionClick={onNextAuctionClick}
               onPrevAuctionClick={onPrevAuctionClick}
             />
+          )}
+        </div>
+
+        <Row className={classes.auctionActivityContainer}>
+          <Col lg={5}>
+            <Row>
+              <Col>
+                <p className={`${classes.noMarginPadding} ${classes.bidText}`}>{`Winning bid`}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col className={classes.ethAddressPadding}>
+                <h3 className={`${classes.noMarginPadding}`}>{`N/ A`}</h3>
+              </Col>
+            </Row>
           </Col>
-        </Row>
-        <Row className={auctionActivityClasses.activityRow}>
-          <Col lg={5} className={auctionActivityClasses.currentBidCol}>
-            <CurrentBid currentBid={BID_N_A} auctionEnded={true} />
-          </Col>
-          <Col
-            lg={5}
-            className={`${auctionActivityClasses.currentBidCol} ${nounContentClasses.currentBidCol}`}
-          >
-            <div className={auctionActivityClasses.section}>
-              <h4>Winner</h4>
-              <h2>nounders.eth</h2>
-            </div>
+          <Col lg={7}>
+            <Row>
+              <Col>
+                <p className={`${classes.noMarginPadding} ${classes.bidText}`}>Winner</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col className={classes.ethAddressPadding}>
+                <h3 className={classes.noMarginPadding}>nounders.eth</h3>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </div>
-      <Row className={auctionActivityClasses.activityRow}>
-        <Col lg={12}>
-          <ul className={auctionBidClasses.bidCollection}>
-            <li className={`${auctionBidClasses.bidRow} ${nounContentClasses.bidRow}`}>
-              All Noun auction proceeds are sent to the{' '}
-              <Link to="/vote" className={nounContentClasses.link}>
-                Nouns DAO
-              </Link>
-              . For this reason, we, the project's founders (‘Nounders’) have chosen to compensate
-              ourselves with Nouns. Every 10th Noun for the first 5 years of the project will be
-              sent to our multisig (5/10), where it will be vested and distributed to individual
-              Nounders.
-            </li>
-          </ul>
-          <div className={bidBtnClasses.bidHistoryWrapper}>
-            <Link to="/nounders" className={bidBtnClasses.bidHistory}>
-              Learn More →
-            </Link>
-          </div>
-        </Col>
-      </Row>
-    </AuctionActivityWrapper>
+    </>
+    // <AuctionActivityWrapper>
+    //   <div className={auctionActivityClasses.informationRow}>
+    //     <Row className={auctionActivityClasses.activityRow}>
+    //       <Col lg={12}>
+    //         <AuctionActivityDateHeadline startTime={mintTimestamp} />
+    //       </Col>
+    //       <Col lg={12} className={auctionActivityClasses.colAlignCenter}>
+    //         <AuctionActivityNounTitle nounId={nounId} />
+    //         <AuctionNavigation
+    //           isFirstAuction={isFirstAuction}
+    //           isLastAuction={isLastAuction}
+    //           onNextAuctionClick={onNextAuctionClick}
+    //           onPrevAuctionClick={onPrevAuctionClick}
+    //         />
+    //       </Col>
+    //     </Row>
+    //     <Row className={auctionActivityClasses.activityRow}>
+    //       <Col lg={5} className={auctionActivityClasses.currentBidCol}>
+    //         <CurrentBid currentBid={BID_N_A} auctionEnded={true} />
+    //       </Col>
+    //       <Col
+    //         lg={5}
+    //         className={`${auctionActivityClasses.currentBidCol} ${nounContentClasses.currentBidCol}`}
+    //       >
+    //         <div className={auctionActivityClasses.section}>
+    //           <h4>Winner</h4>
+    //           <h2>nounders.eth</h2>
+    //         </div>
+    //       </Col>
+    //     </Row>
+    //   </div>
+    //   <Row className={auctionActivityClasses.activityRow}>
+    //     <Col lg={12}>
+    //       <ul className={auctionBidClasses.bidCollection}>
+    //         <li className={`${auctionBidClasses.bidRow} ${nounContentClasses.bidRow}`}>
+    //           All Noun auction proceeds are sent to the{' '}
+    //           <Link to="/vote" className={nounContentClasses.link}>
+    //             Nouns DAO
+    //           </Link>
+    //           . For this reason, we, the project's founders (‘Nounders’) have chosen to compensate
+    //           ourselves with Nouns. Every 10th Noun for the first 5 years of the project will be
+    //           sent to our multisig (5/10), where it will be vested and distributed to individual
+    //           Nounders.
+    //         </li>
+    //       </ul>
+    //       <div className={bidBtnClasses.bidHistoryWrapper}>
+    //         <Link to="/nounders" className={bidBtnClasses.bidHistory}>
+    //           Learn More →
+    //         </Link>
+    //       </div>
+    //     </Col>
+    //   </Row>
+    // </AuctionActivityWrapper>
   );
 };
 export default NounderNounContent;
