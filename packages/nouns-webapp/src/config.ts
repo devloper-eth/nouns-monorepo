ad202ab480d6af9c3b918526f27d329c0bf2bec5
devloper-eth <89419317+devloper-eth@users.noreply.github.com>
2021-09-16 18:38:41 -0700

    change contract address

--- packages/nouns-webapp/src/config.ts
+++ packages/nouns-webapp/src/config.ts
@@ -34,7 +34,7 @@ const config: Record<SupportedChains, Config> = {
     nounsDaoExecutorAddress: '0xd1C753D9A23eb5c57e0d023e993B9bd4F5086b04',
     subgraphApiUri: 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph-rinkeby-v4',
     enableHistory: process.env.REACT_APP_ENABLE_HISTORY === 'true' || true,
-    nounsPartyAddress: '0xaEF3AFf694224217a355154Ee73bD6378C6fAcD0',
+    nounsPartyAddress: '0x238F74D4998536198F5bd35b46941f8fce30D5a1',
   },
   [ChainId.Mainnet]: {
     auctionProxyAddress: '0x830BD73E4184ceF73443C15111a1DF14e495C706',
