85d6260d74b23887dee8448aff3c08748ae2e9f3
devloper-eth <89419317+devloper-eth@users.noreply.github.com>
2021-09-21 22:11:19 -0700

    update footer

--- packages/nouns-webapp/src/components/Footer/index.tsx
+++ packages/nouns-webapp/src/components/Footer/index.tsx
@@ -1,15 +1,11 @@
 import classes from './Footer.module.css';
 import { useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
-import { buildEtherscanAddressLink } from '../../utils/etherscan';
 import { externalURL, ExternalURL } from '../../utils/externalURL';
-import config from '../../config';
 import Section from '../../layout/Section';
 
 const Footer = () => {
-  const twitterURL = externalURL(ExternalURL.twitter);
   const discordURL = externalURL(ExternalURL.discord);
-  const etherscanURL = buildEtherscanAddressLink(config.tokenAddress);
 
   const location = useLocation();
   const useGreyBg = useAppSelector(state => state.application.useGreyBackground);
@@ -36,11 +32,8 @@ const Footer = () => {
 
       <Section bgColor={bgColor} fullWidth={false}>
         <footer className={classes.footerSignature}>
-          <a href={twitterURL} target="_blank" rel="noreferrer">
+          <a href="https://twitter.com/devloper_eth" target="_blank" rel="noreferrer">
             twitter
-          </a>
-          <a href={etherscanURL} target="_blank" rel="noreferrer">
-            etherscan
           </a>
           <a href={discordURL} target="_blank" rel="noreferrer">
             discord
